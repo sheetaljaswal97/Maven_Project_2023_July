7b8d9532fae5d26d8eaa6092cdde55f82224c42e
Sheetal.jaswal <Sheetal.jaswal@gmail.com>
2023-07-11 20:22:47 +0530

    first commit

---
+++ Maven_projects/src/main/java/First_programs/first.java
@@ -0,0 +1,37 @@
+package First_programs;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class first {
+	
+	@Test
+	public void method() throws InterruptedException
+	{
+		
+
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\hp\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+        option.addArguments("start-maximized"); 
+        option.addArguments("--incognito");
+        DesiredCapabilities capabilities= new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, option);
+        capabilities.setBrowserName("chrome");
+        option.merge(capabilities);
+        WebDriver  driver = new ChromeDriver(option);
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Thread.sleep(10000);
+        driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//*[@name='password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+        
+
+	}
+
+
+}
