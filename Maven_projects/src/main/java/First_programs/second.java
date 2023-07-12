a613b5e1992acb4eb443380b011c1a213dc8c195
Sheetal.jaswal <Sheetal.jaswal@gmail.com>
2023-07-12 21:38:54 +0530

    first commit

---
+++ Maven_projects/src/main/java/First_programs/second.java
@@ -0,0 +1,23 @@
+package First_programs;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class second {
+	
+	@Test
+	public void method() throws InterruptedException
+	{
+		
+
+		System.out.println("Second program");
+        
+
+	}
+
+
+}
