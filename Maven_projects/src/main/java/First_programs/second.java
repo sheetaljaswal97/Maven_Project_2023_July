0706b98e1e36e89416e855adcca017bfa01ef5c1
Sheetal.jaswal <Sheetal.jaswal@gmail.com>
2023-07-19 21:51:42 +0530

    first class commit

--- Maven_projects/src/main/java/First_programs/second.java
+++ Maven_projects/src/main/java/First_programs/second.java
@@ -2,8 +2,10 @@ package First_programs;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
@@ -12,11 +14,27 @@ public class second {
 	@Test
 	public void method() throws InterruptedException
 	{
-		
-
-		System.out.println("Second program");
-        
-
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\hp\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--remote-allow-origins=*");
+        option.addArguments("start-maximized"); 
+        option.addArguments("--incognito");
+        DesiredCapabilities capabilities= new DesiredCapabilities();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, option);
+        capabilities.setBrowserName("chrome");
+        option.merge(capabilities);
+        WebDriver  driver = new ChromeDriver(option);
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Thread.sleep(10000);
+        driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//*[@name='password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+        Thread.sleep(10000);
+        WebElement admin=driver.findElement(By.xpath("//li[@class='oxd-main-menu-item-wrapper']/a//following::span[text()='Admin']"));
+        Thread.sleep(10000);
+        Actions action = new Actions(driver);
+        action.moveToElement(admin).perform();
+        action.click(admin).build().perform();
 	}
 
 
